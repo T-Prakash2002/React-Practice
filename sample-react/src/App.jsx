ec34a6eca1929190f5fa5580f716897bb85e1525
T-Prakash2002 <prakasht1405@gmail.com>
2024-08-13 13:38:54 +0530

    set Count

--- sample-react/src/App.jsx
+++ sample-react/src/App.jsx
@@ -1,13 +1,23 @@
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
 import './App.css'
 
 function App() {
+  const [count, setCount] = useState(0);
+ useEffect(() => {
+	console.log("Component rendered successfully");
+  }, []);
+   return (
+	<div>
+  	<button onClick={() => setCount(count + 1)}>Click me</button>
+    <Display count={count} />
+	</div>
+  );
+}
 
-  return (
-    <>
-      
-    </>
-  )
+function Display({count}){
+    	return(
+        <p>You clicked {count} times</p>
+      )
 }
 
 export default App
